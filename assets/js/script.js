b2957cb91d3c99d8d84a8eefc795448e17478cbb
derekmcauley <derekmcauley7@gmail.com>
2023-04-29 09:41:46 +0100

    Adding addition question check

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") == "submit") {
-                alert(`You clicked submit!!!`);
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -28,10 +28,27 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
-
+    let correctAnswer = calculateCorrectAnswer();
+    let guess = parseInt(document.getElementById('answer-box').value);
+    if (correctAnswer[0] === guess) {
+        alert('Correct Answer!!!');
+    } else {
+        alert(`Sorry the answer is ${correctAnswer}, you guesses ${guess}`);
+    }
 }
 
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
@@ -47,7 +64,7 @@ function displayAdditionQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('opterator').textContent = "+";
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
